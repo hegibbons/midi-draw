dfe253721b79791e40d63bfd7205621864e3b8f1
Harry Gibbons <harry.gibbons@tout.com>
2016-09-04 11:30:15 -0700

    Update midi-draw.js

--- midi-draw.js
+++ midi-draw.js
@@ -10,7 +10,7 @@ $( document ).ready(function() {
       mod += 0.01
 
       console.log(mod, increase)
-      var canvas = document.getElementById('acanvas');
+      var canvas = document.getElementById('canvas');
       var context = canvas.getContext('2d');
       var center = 250;
       var bigRadius = center * .8;
@@ -59,6 +59,43 @@ $( document ).ready(function() {
       
       if (mod < 1) {
       requestAnimationFrame(drawMidi);
+      } else {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+              context.beginPath();
+      context.arc(center, center, bigRadius, (6.75 * pi) / 12, (5.2 * pi) / 12);
+      context.stroke();
+      context.closePath();
+
+      //top dot
+      context.beginPath();
+      context.arc(center, center * .53, smallRadius, 0, 2 * pi);
+      context.stroke();
+  
+      //mid dots
+      //left
+      context.beginPath();
+      context.arc(thirdPos, thirdPos, smallRadius, 0, 2 * pi);
+      context.stroke();
+      
+      //right
+      context.beginPath();
+      context.arc(center * 4/3, thirdPos, smallRadius, 0, 2 * pi);
+      context.stroke();
+      
+      //low dots
+      //left
+      context.beginPath();
+      context.arc(center * .53, center, smallRadius, 0, 2 * pi);
+      context.stroke();
+      //right
+      context.beginPath();
+      context.arc(center * 1.465, center, smallRadius, 0, 2 * pi);
+      context.stroke();
+      
+      //bottom semicircle
+      context.beginPath();
+      context.arc(center, center * 1.79, bigRadius / 5, pi, 2 * pi);
+      context.stroke();  
       }
   }
 
